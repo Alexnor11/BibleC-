c03afce691be6c41fd843d1750b82d4499cbde34
Alexnor11 <advance114@gmail.com>
2026-01-14 11:05:14 +0300

    add WeatheAP

--- Basics/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
+++ Basics/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159940d1cfab61863c2177620ac8e6341e0ff771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116dd7d1406f4b010ca68a5fbe96fddff41d6256")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TestWeatheAPI/TestWeatheAPI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestWeatheAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string apiKey = "5869LJUHW5XPFWVFP9KN9UUQV";
+            string location = "Paris"; // Example location
+            string url = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?unitGroup=metric&contentType=json&key={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    dynamic weatherData = JsonConvert.DeserializeObject(responseBody);
+
+                    foreach (var day in weatherData.days)
+                    {
+                        Console.WriteLine($"Date: {day.datetime}");
+                        Console.WriteLine($"Max Temp: {day.tempmax}");
+                        Console.WriteLine($"Min Temp: {day.tempmin}");
+                        Console.WriteLine($"Description: {day.description}");
+                        Console.WriteLine("--------------------------------");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request error: {e.Message}");
+                }
+            }
+        }
+    }
+}
